70b238ea418e6e4852936e7305698a9f12fb8da0
Cipriano <professorpedrocipriano@gmail.com>
2024-12-13 00:37:02 -0300

    Add files via upload

---
+++ Course/10-010_Wifi_connection/10-010_Wifi_connection.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+
+const char* ssid     = "ardwifi";
+const char* password = "ardwifi987";
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+
+}
